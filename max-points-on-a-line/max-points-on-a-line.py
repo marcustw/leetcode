47dd66cb612323ed8b54fee2316ef44005f28547
Marcus <e0014884@u.nus.edu>
2021-06-19 17:48:19 +0800

    Runtime: 168 ms, Memory Usage: 14.3 MB,

--- max-points-on-a-line/max-points-on-a-line.py
+++ max-points-on-a-line/max-points-on-a-line.py
@@ -3,49 +3,25 @@ class Solution:
         length = len(points)
         if (length <= 2):
             return length;
-        else:
-            largest = 1
-            mem = {}
-            vert = {}            
-            for i in range(0, length):
-                for j in range(i, length):
-                    a = points[i]
-                    b = points[j]
-                    dy = a[1] - b[1]
-                    dx = a[0] - b[0]
-                    if (dx == 0):
-                        x = a[0]
-                        if x not in vert:
-                            count = 0
-                            for point in points:
-                                if point[0] == x:
-                                    count = count + 1
-                            vert[x] = count
-                            if count > largest:
-                                largest = count
-                    else:
-                        gradient = float(dy)/dx
-                        c1 = b[1] - (gradient * b[0])
-                        c2 = a[1] - (gradient * a[0])
-                        cavg = (c1 + c2) / 2.0
-                        c = round(cavg, 12)
-                        ground = round(gradient, 12)
-                        if ground not in mem:
-                            mem[ground] = {}
-                        if c not in mem[ground]:
-                            count = 0
-                            #print('===========')
-                            #print('m = ', gradient, '  , c= ', c)
-                            for point in points:
-                                x = point[0]
-                                y = point[1]
-                                #print('===========')
-                                if (abs(y - ((gradient * x) + cavg)) <= 0.000000000001):
-                                    #print(point)
-                                    #print(y - ((gradient * x) + c))
-                                    count = count + 1
-                            mem[ground][c] = count
-                            if count > largest:
-                                largest = count
-            # print(mem)
-            return largest
+        largest = 0
+        for i in range(0, length - 2):
+            p1 = points[i]
+            x1 = p1[0]
+            y1 = p1[1]
+            
+            
+            for j in range(i + 1, length - 1):
+                p2 = points[j]
+                x2 = p2[0]
+                y2 = p2[1]
+                
+                count = 2
+                for k in range(j + 1, length):
+                    p3 = points[k]
+                    x3 = p3[0]
+                    y3 = p3[1]
+                    
+                    if (((y3 - y1) * (x2 - x1)) == ((y2 - y1) * (x3 - x1))):
+                        count += 1
+                largest = max(count, largest)
+        return largest
