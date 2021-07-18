8d7a3f65cc26ef4e6bc111e92125e58424f917e5
Marcus <e0014884@u.nus.edu>
2021-07-18 21:40:48 +0800

    Time: 21 ms (5.08%), Space: 39.8 MB (23.24%) - LeetHub

---
+++ add-two-numbers/add-two-numbers.java
@@ -0,0 +1,41 @@
+import java.math.*;
+
+public class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        StringBuilder x1 = new StringBuilder();
+        while (l1 != null) {
+            x1.insert(0, l1.val);
+            l1 = l1.next;
+        }
+
+        StringBuilder x2 = new StringBuilder();
+        while (l2 != null) {
+            x2.insert(0, l2.val);
+            l2 = l2.next;
+        }
+
+        BigInteger first = new BigInteger(x1.toString());
+        BigInteger second = new BigInteger(x2.toString());
+        BigInteger sum = first.add(second);
+
+        String s = sum.toString();
+        ListNode result = new ListNode();
+
+        System.out.println(x1.toString());
+        System.out.println(x2.toString());
+        System.out.println(s);
+
+        ListNode curResult = result;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            curResult.val = Integer.parseInt(String.valueOf(s.charAt(i)));
+            if (i == 0) {
+                curResult.next = null;
+            } else {
+                curResult.next = new ListNode();
+            }
+            curResult = curResult.next;
+        }
+        return result;
+    }
+}
