4c25b42bb1958ad52c88e632dc016d6061f89713
Marcus <e0014884@u.nus.edu>
2021-12-21 21:42:08 +0800

    Time: 376 ms (9.08%), Space: 60.8 MB (6.53%) - LeetHub

---
+++ longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        // sort list according to length of string (biggest comes first)
+        sort(words.begin(), words.end(),  []
+            (const std::string& first, const std::string& second){
+                return first.size() > second.size();
+            });
+        
+        map<string, int> mem;
+        
+        for (string x : words) {
+            mem[x] = 1;
+        }
+        
+        for (string x : words) {
+            for (int i = 0; i < x.size(); i++) {
+                string t = x;
+                t.erase(t.begin()+i);
+                mem[t] = max(mem[t], 1 + mem[x]);
+            }
+        }
+        int res = 0;
+        for (string x : words) {
+            res = max(res, mem[x]); 
+        }
+        
+        return res;
+    }
+};
