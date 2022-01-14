40c8259e9a0027b9bf9eeba33473af14312ecb52
Marcus <e0014884@u.nus.edu>
2022-01-14 21:36:49 +0800

    Time: 3 ms (76.97%), Space: 15 MB (26.08%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void backtrack(int open, int close, string s, vector<string>& res) {
+        if (close < open || close < 0 || open < 0) return;
+        if (open == 0 && close == 0) {
+            res.push_back(s);
+            return;
+        }
+        backtrack(open-1, close, s+"(", res);
+        backtrack(open, close-1, s+")", res);
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        backtrack(n, n, "", res);
+        return res;
+    }
+};
