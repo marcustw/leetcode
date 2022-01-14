74ffba923381c389aea27de42cc987b6322039cb
Marcus <e0014884@u.nus.edu>
2022-01-14 21:09:30 +0800

    Time: 69 ms (5.62%), Space: 14.2 MB (81.95%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,37 @@
+class Solution:
+    
+    def myAtoi(self, s: str) -> int:
+        lowerLimit = - 2 ** 31
+        upperLimit = (2 ** 31) - 1
+        ans = 0
+        isNegative = False
+        hasSign = False
+        hasDigits = False
+        for i in range(len(s)):
+            if (s[i] != '-' and s[i] != '+' and s[i] != ' ' and not s[i].isnumeric()):
+                break
+            else:
+                if (s[i].isnumeric()):
+                    hasDigits = True
+                if (s[i] == ' '):
+                    if (hasDigits or hasSign):
+                        break
+                    else:
+                        continue
+                if (s[i] == '-' or s[i] == '+') and (hasDigits or hasSign):
+                    break
+                if (s[i] == '-'):
+                    isNegative = True
+                    hasSign = True
+                elif (s[i] == '+'):
+                    isNegative = False
+                    hasSign = True
+                else:
+                    ans = ans * 10 + int(s[i])
+        if (isNegative):
+            ans = -1 * ans
+        if ans < lowerLimit:
+            return lowerLimit
+        if ans > upperLimit:
+            return upperLimit
+        return ans
