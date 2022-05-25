a4e21a26b4029123576f56819f14a371a7893609
Marcus <e0014884@u.nus.edu>
2022-05-26 01:01:35 +0800

    Time: 55 ms (6.00%), Space: 6 MB (29.99%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return n!=0 && 3486784401u%n==0;
+        if (n==0) return false;
+        else if (n==1) return true;
+        else return n%3==0 && isPowerOfThree(n/3);
     }
 };
