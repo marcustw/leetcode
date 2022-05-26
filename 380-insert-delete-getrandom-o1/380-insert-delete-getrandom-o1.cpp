a9768ea448c29337152ddbe6c40b756778e5a8e8
Marcus <e0014884@u.nus.edu>
2022-05-26 11:39:21 +0800

    Update 380-insert-delete-getrandom-o1.cpp

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -21,17 +21,10 @@ public:
         m.erase(val);
         return true;
     }
-    
+ 
+    // O(1)
     int getRandom() {
         int ranIdx = rand()%v.size();
         return v[ranIdx];
     }
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
