482d35feeba7f6275b9f965fab82c9b3cd4d7d2b
Marcus <e0014884@u.nus.edu>
2022-05-30 00:27:56 +0800

    Time: 7 ms (27.82%), Space: 6.6 MB (87.74%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-         int ans=0;
+        int ans=0;
+        // sum(t) - sum(s) = diff
         for(char x: t)
             ans+=x;
         for (char x: s)
