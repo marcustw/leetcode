a4df01b0b21a45e1be11eb5750800c0ac5506a5f
Marcus <e0014884@u.nus.edu>
2022-05-30 00:32:51 +0800

    Time: 27 ms (54.45%), Space: 17 MB (53.56%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int> s;
-        for (int n : nums) {
-            if (s.count(n) == 0) s.insert(n);
-            else s.erase(n);
-        }
-        return *s.begin();
+       int ans=0;
+	   for(auto n:nums)
+	   ans^=n;
+	   return ans;
     }
 };
