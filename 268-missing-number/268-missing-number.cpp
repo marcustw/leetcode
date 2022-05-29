fbd842e22e8e949c311403bca04a059a428aeb56
Marcus <e0014884@u.nus.edu>
2022-05-30 00:39:24 +0800

    Time: 39 ms (18.81%), Space: 17.9 MB (65.00%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans=0;
+        for (int i=0; i<= nums.size(); i++) {
+            ans^=i;
+        }
+        for (int n : nums) ans^=n;
+        return ans;
+    }
+};
