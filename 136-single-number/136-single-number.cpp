d0128de537e6bb30c71c272ab7cc29673cd38ff1
Marcus <e0014884@u.nus.edu>
2022-05-30 00:33:29 +0800

    Time: 23 ms (69.86%), Space: 16.9 MB (87.31%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       int ans=0;
-	   for(auto n:nums)
-	   ans^=n;
-	   return ans;
+       return accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
     }
 };
