f34991a918739ad5a40720cb8f43454a179b6206
Marcus <e0014884@u.nus.edu>
2022-06-02 01:02:30 +0800

    Time: 29 ms (23.54%), Space: 14.3 MB (59.45%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -13,8 +13,16 @@ public:
         if (root==NULL || root==p || root==q) return  root;
         TreeNode* left=lowestCommonAncestor(root->left,p,q);
         TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        
+        //if LCA not at the left, it must be at the right
         if (left==NULL) return right;
+        
+        //if LCA not at the right, it must be at the left
         else if (right==NULL) return left;
+        
+        //if "LCAs" are found in both left and right
+        //then there exist an LCA=root that is ancestor of both
+        //left and right!
         return root;
     }
 };
