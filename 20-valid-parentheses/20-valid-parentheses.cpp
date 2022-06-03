0d6d3ddebb39a1926d55debb826cc7462e4fba80
Marcus <e0014884@u.nus.edu>
2022-06-04 01:18:07 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (51.67%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        set<char> left={'(','{', '['};
-        for (int i=0; i<s.length(); i++) {
-            if (left.count(s[i])==1) {
-                stk.push(s[i]);
-            } else if (stk.empty()) {
-                return false;
-            } else if (s[i]==')') {
-                if (stk.top()!='(') return false;
-                stk.pop();
-            } else if (s[i]==']') {
-                if (stk.top()!='[') return false;
-                stk.pop();
-            } else {
-                if (stk.top()!='{') return false;
+        for (auto c:s) {
+            if (c=='(' || c=='[' || c=='{') stk.push(c);
+            else {
+                if (stk.empty()) return false;
+                char k=stk.top();
+                if (
+                    (c==')' && k!='(') ||
+                    (c=='}' && k!='{') ||
+                    (c==']' && k!='[')
+                ) {
+                    return false;
+                }
                 stk.pop();
             }
         }
-        return stk.size()==0;
+        return stk.empty();
     }
 };
