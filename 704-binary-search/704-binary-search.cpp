0b7a1e6cb31862bd9ccb303aa4a5f4343bb9aa0c
Marcus <e0014884@u.nus.edu>
2022-06-04 01:01:29 +0800

    Time: 67 ms (17.99%), Space: 27.5 MB (92.26%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
+        while (l<=r) {
+            int i=(l+r)/2;
+            if (nums[i]==target) return i;
+            else if (nums[i]>target) {
+                r=i-1;
+            } else {
+                l=i+1;
+            }
+        }
+        return -1;
+    }
+};
