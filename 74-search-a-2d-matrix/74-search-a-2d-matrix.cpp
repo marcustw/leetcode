aaafc250293268eeba190dd288d9c21dbbe42c17
Marcus <e0014884@u.nus.edu>
2022-06-09 00:02:50 +0800

    Time: 8 ms (36.21%), Space: 9.6 MB (52.65%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,32 +1,16 @@
 class Solution {
-private:
-    bool binSearch(vector<int>& arr, int target, int n) {
-        int lo=0;
-        int hi=n-1;
-        while(lo<=hi) {
-            int mid=(lo+hi)/2;
-            if (arr[mid]==target) return true;
-            if (target<arr[mid]) hi=mid-1;
-            else lo=mid+1;
-        }
-        return false;
-    }
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int u=0;
-        int d=m-1;
-        while (u<d) {
-            int mid=(u+d)/2;
-            int m0=matrix[mid][0];
-            int m1=matrix[mid][n-1];
-            if (target>=m0 && target<=m1) {
-                return binSearch(matrix[mid],target,n);
-            }
-            if (target>m1) u=mid+1;
-            else d=mid-1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int l = 0, r = m * n - 1;
+        while (l != r){
+            int mid = (l+r)/2;
+            if (matrix[mid / m][mid % m] < target)
+                l = mid + 1;
+            else 
+                r = mid;
         }
-        return binSearch(matrix[u],target,n);
+        return matrix[r / m][r % m] == target;
     }
 };
