8f46f55dfa5c6eb52078e696b51d10ae8edf1de5
Marcus <e0014884@u.nus.edu>
2022-06-09 23:02:26 +0800

    Time: 2 ms (43.51%), Space: 6.2 MB (27.23%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,12 +1,4 @@
 class Solution {
-private:
-    bool isPalindrome(string& s) {
-        int n=s.size();
-        for (int i=0; i<n/2; i++) {
-            if (s[i]!=s[n-i-1]) return false;
-        }
-        return true;
-    }
 public:
     /*
         - Idea is in a single step, you can remove a palindromic subsequence
@@ -16,6 +8,10 @@ public:
         - If s is not palindrome, remove all 'a' as palindromic subsequence, then remove all 'b'
     */
     int removePalindromeSub(string s) {
-        return isPalindrome(s) ? 1 : 2;
+        int n=s.size();
+        for (int i=0; i<n/2; i++) {
+            if (s[i]!=s[n-i-1]) return 2;
+        }
+        return 1;
     }
 };
