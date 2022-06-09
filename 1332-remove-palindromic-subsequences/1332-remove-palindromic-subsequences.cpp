9af4bf7d57ec48b900885d4a2d216b841ba70e24
Marcus <e0014884@u.nus.edu>
2022-06-09 22:59:39 +0800

    Time: 0 ms (100.00%), Space: 6 MB (97.71%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -8,6 +8,13 @@ private:
         return 0;
     }
 public:
+    /*
+        - Idea is in a single step, you can remove a palindromic subsequence
+        - if s is empty, do not need to remove
+            - However, constraints given s.length() >= 1 so s will never be empty
+        - If s is palindrome, just remove it
+        - If s is not palindrome, remove all 'a' as palindromic subsequence, then remove all 'b'
+    */
     int removePalindromeSub(string s) {
         return 1+isPalindrome(s);
     }
