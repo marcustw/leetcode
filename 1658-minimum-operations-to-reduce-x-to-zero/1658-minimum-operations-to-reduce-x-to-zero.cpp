a56b442656f2af4017db495dd4ee6ec3114261b9
Marcus <e0014884@u.nus.edu>
2022-06-12 00:31:24 +0800

    Time: 1248 ms (5.09%), Space: 217.1 MB (5.08%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,18 +2,26 @@ class Solution {
 public:    
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
-        int sum1=0,sum2=0;
-        for (int i=0; i<n; i++) sum2+=nums[i];
-        sum2-=x;
-        
-        if (sum2==0) return n; //sum off whole array adds up to x
-        
-        int i=0, ans=0;
-        for (int j=0; j<n; j++) {
-            sum1+=nums[j];
-            while (i<n && sum1>sum2) sum1-=nums[i++];
-            if (sum1==sum2) ans=max(ans,j-i+1);
+        unordered_map<int, int> m;
+        vector<int> ltr(n,nums[0]);
+        vector<int> rtl(n,nums[n-1]);
+        m[nums[n-1]]=1;
+        for (int i=1; i<n; i++) ltr[i]=ltr[i-1]+nums[i];
+        for (int j=n-2; j>=0; j--) {
+            rtl[j]=rtl[j+1]+nums[j];
+            if (!m.count(rtl[j])) {
+                m[rtl[j]]=n-j;
+            } else {
+                m[rtl[j]]=min(m[rtl[j]],n-j);
+            }
         }
-        return ans==0 ? -1 : n-ans;
+        int ans=INT_MAX;
+        for (int i=0; i<n; i++) {
+            int y=ltr[i];
+            if (y==x) ans=min(ans,i+1);
+            else if (m[x-y]) ans=min(ans,i+1+m[x-y]);
+        }
+        if (m[x]) ans=min(ans,m[x]);
+        return ans>n ? -1 : ans;
     }
 };
