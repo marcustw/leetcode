cd5f794dbb03a2b03ae6ca7fd83b8c7577526c63
Marcus <e0014884@u.nus.edu>
2022-06-11 17:12:21 +0800

    Time: 74 ms (9.83%), Space: 7.1 MB (15.38%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,20 +1,20 @@
 class Solution {
+private:
+    static bool compare(const pair<int,int>&a, const pair<int,int>&b) {
+       return a.second<b.second;
+    }
 public:
     int characterReplacement(string s, int k) {
         int ans=0;
         unordered_map<int,int> m;
-        int maxfreq=0;
         int i=0;
         for (int j=0; j<s.length(); j++) {
             m[s[j]]++;
-            maxfreq=max(maxfreq,m[s[j]]);
-            if (j-i+1-maxfreq>k) {
+            int cmax=max_element(m.begin(),m.end(),compare)->second;
+            if (j-i+1-cmax>k) {
                 m[s[i]]--;
                 i++;
-            } else {
-                ans=max(ans,j-i+1);
-            }
-              
+            } else ans=max(ans,j-i+1);    
         }
         return ans;
     }
