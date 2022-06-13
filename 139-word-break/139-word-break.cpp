b902bfa7aa4ce4100a56b6539b2920f2fbfcb30b
Marcus <e0014884@u.nus.edu>
2022-06-14 00:08:26 +0800

    Time: 6 ms (81.67%), Space: 7.6 MB (94.87%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.length();
+        vector<bool> dp(n+1);
+        dp[n]=true;
+        for (int i=n-1; i>=0; i--) {
+            for (auto w : wordDict) {
+                if (i+w.length() <= s.length() && s.substr(i,w.length())==w)
+                    dp[i]=dp[i+w.length()];
+                if (dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+};
