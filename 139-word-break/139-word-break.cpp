a7e711a1adbb515ac3fa1c0c18dccd598c0b2cc5
Marcus <e0014884@u.nus.edu>
2022-06-14 00:09:59 +0800

    Time: 4 ms (88.88%), Space: 7.6 MB (94.87%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -6,6 +6,7 @@ public:
         dp[n]=true;
         for (int i=n-1; i>=0; i--) {
             for (auto w : wordDict) {
+                //if you are able to form a string w from index i of string s, set dp[i]=dp[i+w.length()]
                 if (i+w.length() <= s.length() && s.substr(i,w.length())==w)
                     dp[i]=dp[i+w.length()];
                 if (dp[i]) break;
