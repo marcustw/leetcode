f3bf702496d7597e6b076ba007f39dc138e4d1e1
Marcus <e0014884@u.nus.edu>
2022-06-13 23:31:01 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (65.30%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -4,8 +4,9 @@ public:
         long l=1, r=num;
         while (l<=r) {
             long m=(l+r)/2;
-            if (m*m==num) return true;
-            else if (m*m>num) r=m-1;
+            long m2=m*m;
+            if (m2==num) return true;
+            else if (m2>num) r=m-1;
             else l=m+1;
         }
         return false;
