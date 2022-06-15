a56c58c18c87e9e460d7f3fb60eed7bb049a11d9
Marcus <e0014884@u.nus.edu>
2022-06-15 13:13:43 +0800

    Time: 270 ms (18.00%), Space: 61.5 MB (6.87%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        // sort list according to length of string (biggest comes first)
-        sort(words.begin(), words.end(),  []
-            (const std::string& first, const std::string& second){
-                return first.size() > second.size();
-            });
+        // sort the strings according to length
+        sort(words.begin(),words.end(),[]
+             (auto& a, auto& b) {
+                 return a.length()>b.length();
+             });
         
-        unordered_map<string, int> mem;
+        // dp
+        unordered_map<string,int> m;
         
-        for (string x : words) mem[x] = 1;
+        // initialise dp
+        for (string word : words) m[word]=1;
         
-        for (string x : words) {
-            for (int i = 0; i < x.size(); i++) {
-                string t = x;
-                t.erase(t.begin()+i);
-                mem[t] = max(mem[t], 1 + mem[x]);
+        int ans=0;
+        
+        /*
+        For each word in words
+            form a string wordMinusCharC by removing a char C from word
+            if wordMinusChar exists in map, a chain from 
+                wordMinusCharC + C -> word
+                find max length of chain that you can delete to form wordMinusCharC
+        
+            # Note that max length of chain that you can delete to form wordMinusCharC = max length of chain you can add from wordMinusCharC to form a word existing in words
+                
+        */
+        for (auto& word : words) {
+            for (int i=0; i<word.length(); i++) {
+                string wordMinusCharC=word;
+                wordMinusCharC.erase(i,1);
+                m[wordMinusCharC]=max(m[wordMinusCharC],1+m[word]);
             }
+            ans=max(m[word],ans);
         }
-        int res = 0;
-        for (string x : words) res = max(res, mem[x]); 
-        return res;
+        return ans;
     }
 };
