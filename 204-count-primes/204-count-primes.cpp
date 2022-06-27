b2e0b3fc24e8c627a41ed659b121672fc621613a
Marcus <e0014884@u.nus.edu>
2022-06-27 23:36:33 +0800

    Time: 301 ms (53.46%), Space: 10.2 MB (69.75%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -4,13 +4,12 @@ public:
         if (n<=2) return 0;
         vector<bool> nones(n,false);
         int ans=1;
-        int upper=(int)sqrt(n);
         for (int i=3; i<n; i+=2) {
-            if (nones[i]) continue;
-            ans++;
-            if (i>upper) continue;
-            for (int j=i*i; j<n; j+=2*i) {
-                nones[j]=true;
+            if (!nones[i]) {
+                ans++;
+                for (int j=2; j*i<n; j++) {
+                    nones[j*i]=true;
+                }
             }
         }
         return ans;
