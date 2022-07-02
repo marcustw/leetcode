2e4446024cb2f2f6339c9fcd4bb0210da9848c89
Marcus <e0014884@u.nus.edu>
2022-07-02 10:20:25 +0800

    Time: 79 ms (92.25%), Space: 32.4 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -3,14 +3,14 @@ public:
     int maxArea(int h, int w, vector<int>& hs, vector<int>& vs) {
         sort(hs.begin(),hs.end());
         sort(vs.begin(),vs.end());
-        hs.insert(hs.begin(),0); hs.push_back(h);
-        vs.insert(vs.begin(),0); vs.push_back(w);
-        int maxH=0;
-        int maxW=0;
+        hs.push_back(h);
+        vs.push_back(w);
+        int maxH=hs[0];
+        int maxW=vs[0];
         for (int i=0; i<hs.size()-1; i++) maxH=max(hs[i+1]-hs[i],maxH);
         for (int j=0; j<vs.size()-1; j++) maxW=max(vs[j+1]-vs[j],maxW);
-        long long H = (long long) maxH;
-        long long W = (long long) maxW;
+        long long int H=(long long int)maxH;
+        long long int W=(long long int)maxW;
         return (int) ((H*W) % 1000000007);
     }
 };
