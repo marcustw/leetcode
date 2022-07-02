4c86108a3f80bd371e1d9ec124c3130139e0c281
Marcus <e0014884@u.nus.edu>
2022-07-02 10:18:01 +0800

    Time: 92 ms (68.41%), Space: 32.2 MB (76.96%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,33 +1,16 @@
-class Solution {
-    long long const mod=1000000007;
-    
-    long long moduloMultiplication(long long a, long long b, long long mod) {
-        long long res = 0; 
-        a %= mod;
-        while (b) {
-            if (b & 1)
-                res = (res + a) % mod;
-            a = (2 * a) % mod;
-            b >>= 1;
-        }
-        return res;
-    }
+class Solution {    
 public:
     int maxArea(int h, int w, vector<int>& hs, vector<int>& vs) {
         sort(hs.begin(),hs.end());
         sort(vs.begin(),vs.end());
         hs.insert(hs.begin(),0); hs.push_back(h);
         vs.insert(vs.begin(),0); vs.push_back(w);
-        long long maxH=0;
-        long long maxW=0;
-        for (int i=0; i<hs.size()-1; i++) {
-            long long l=hs[i+1]-hs[i];
-            maxH=max(l,maxH);
-        }
-        for (int j=0; j<vs.size()-1; j++) {
-            long long b=vs[j+1]-vs[j];
-            maxW=max(b,maxW);
-        }
-        return (int) moduloMultiplication(maxH,maxW,mod);
+        int maxH=0;
+        int maxW=0;
+        for (int i=0; i<hs.size()-1; i++) maxH=max(hs[i+1]-hs[i],maxH);
+        for (int j=0; j<vs.size()-1; j++) maxW=max(vs[j+1]-vs[j],maxW);
+        long long H = (long long) maxH;
+        long long W = (long long) maxW;
+        return (int) ((H*W) % 1000000007);
     }
 };
