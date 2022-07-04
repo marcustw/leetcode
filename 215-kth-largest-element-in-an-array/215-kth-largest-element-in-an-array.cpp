e905686a9b9affae01193d0e26372f330c3e462c
Marcus <e0014884@u.nus.edu>
2022-07-04 21:48:51 +0800

    Time: 15 ms (42.71%), Space: 10.1 MB (36.86%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -5,7 +5,8 @@ public:
         int n=nums.size();
         for (int i=0;i<n;i++) {
             pq.push(nums[i]);
-            if (pq.size()>k) pq.pop();
+            if (pq.size()>k)
+                pq.pop();
         }
         return pq.top();
     }
