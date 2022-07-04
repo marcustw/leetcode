c3d3c547dff66b9e43e3d86c543820f7924ad1a3
Marcus <e0014884@u.nus.edu>
2022-07-04 22:05:07 +0800

    Time: 15 ms (42.71%), Space: 10.1 MB (36.86%) - LeetHub (w/ Time Complexity)
    Add Time Complexity

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+    // O(nlog(k)) time complexity
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq ;
+        priority_queue<int,vector<int>,greater<int>> pq;
         int n=nums.size();
+        // iterate through n elements
         for (int i=0;i<n;i++) {
             pq.push(nums[i]);
+            
+            // always maintaining a minHeap of k elements
             if (pq.size()>k)
-                pq.pop();
+                pq.pop(); // O(log k)
         }
         return pq.top();
     }
