199e3ae9def49f3fb90fb0c51b8e997858d751fd
Marcus <e0014884@u.nus.edu>
2022-07-05 23:19:09 +0800

    Time: 3 ms (72.41%), Space: 7.3 MB (55.64%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,18 @@
-#include <algorithm>
-
 class Solution {
-public:
-    void helper(vector<vector<int>>& res, vector<int>& nums, int index, vector<int> subset) {
-        
-        res.push_back(subset);
-        
-        for (int i = index; i < nums.size(); i++) {
-            subset.push_back(nums[i]);
-            helper(res, nums, i+1, subset);
-            subset.pop_back();
+    vector<vector<int>> ans;
+    
+    void recurse(vector<int>& nums, vector<int> v, int index) {
+        ans.push_back(v);
+        for (int i=index; i<nums.size(); i++) {
+            v.push_back(nums[i]);
+            recurse(nums,v,i+1);
+            v.pop_back();
         }
     }
-    
+public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> subset;
-        helper(res, nums, 0, subset);
-        return res;
+        vector<int> v;
+        recurse(nums,v,0);
+        return ans;
     }
 };
