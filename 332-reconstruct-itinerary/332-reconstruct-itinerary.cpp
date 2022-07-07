80a15b77759f820554512736c455dd631a86fc86
Marcus <e0014884@u.nus.edu>
2022-07-07 23:43:20 +0800

    Time: 67 ms (13.48%), Space: 15.5 MB (25.55%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -1,12 +1,13 @@
 class Solution {
     vector<string> ans;
+    // key=string, value=minHeap<string>
     unordered_map<string,priority_queue<string,vector<string>,greater<string>>> edges;
     
     void recurse(string airport) {
-        while (edges[airport].size()) {
+        while (edges[airport].size()) { // destinations sorted in lexicographical order
             string next = edges[airport].top();
             edges[airport].pop();
-            recurse(next);
+            recurse(next); // explore smaller neighbour first
         }
         ans.insert(ans.begin(),airport);
     }
