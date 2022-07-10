e9a5f7e15771d722659bcb5eba6336774c23157e
Marcus <e0014884@u.nus.edu>
2022-07-10 17:24:10 +0800

    Time: 48 ms (90.40%), Space: 34.7 MB (50.03%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,24 +1,13 @@
 class Solution {
-    vector<bool> visited;
-    vector<bool> can;
-    
     bool recurse(vector<int>& arr, int i) {
         if (i<0 || i>=arr.size()) return false;
-        else if (visited[i]) return can[i];
-        else if (arr[i]==0) {
-            visited[i]=true;
-            can[i]=true;
-            return true;
-        }
-        visited[i] = true;
-        can[i] = recurse(arr, i-arr[i]) || recurse(arr, i+arr[i]);
-        return can[i];
+        if (arr[i]<0) return false;
+        if (arr[i]==0) return true;
+        arr[i]=-arr[i];
+        return recurse(arr,i-arr[i]) || recurse(arr,i+arr[i]);
     }
 public:
     bool canReach(vector<int>& arr, int start) {
-        if (arr[start]==0) return true;
-        visited=vector<bool>(arr.size(), false);
-        can=vector<bool>(arr.size(), false);
-        return recurse(arr, start);
+        return recurse(arr,start);
     }
 };
