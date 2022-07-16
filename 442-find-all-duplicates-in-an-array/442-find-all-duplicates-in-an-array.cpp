3d05d231e0c92614e5a6e55de338bde14f86d17a
Marcus <e0014884@u.nus.edu>
2022-07-16 23:47:54 +0800

    Time: 104 ms (47.30%), Space: 33.7 MB (48.89%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -5,6 +5,7 @@ public:
         for(int i=0; i<nums.size(); i++){
             int j=abs(nums[i])-1;
             nums[j]*=-1;
+            // if item (nums[j]) happens to be double negated, then it means j has been repeated twice.
             if (nums[j]>0) ans.push_back(abs(nums[i]));
         }
         return ans;
