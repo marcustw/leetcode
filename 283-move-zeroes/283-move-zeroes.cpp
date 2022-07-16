c92cb2f2e411e63f54a84cd823376ebe8a71fd2c
Marcus <e0014884@u.nus.edu>
2022-07-16 13:09:25 +0800

    Time: 31 ms (69.89%), Space: 19.3 MB (13.43%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> clone(nums.begin(), nums.end());
-        int n=nums.size();
         int start=0;
-        int end=n-1;
-        for (int i=0; i<n; i++) {
-            if (clone[i]==0)
-                nums[end--]=0;
-            else
-                nums[start++]=clone[i];
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i]) nums[start++]=nums[i];
+            
         }
+        while (start<nums.size()) nums[start++]=0;
     }
 };
