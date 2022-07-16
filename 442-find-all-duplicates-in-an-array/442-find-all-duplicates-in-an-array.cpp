f7e522093d30f5e0acf4a0c15df6fbb216b5b9d4
Marcus <e0014884@u.nus.edu>
2022-07-16 23:38:14 +0800

    Time: 124 ms (31.37%), Space: 44.6 MB (18.26%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        unordered_set<int> sed;
+        vector<int> ans;
+        for (auto& x : nums) {
+            if (sed.count(x)) {
+                ans.push_back(x);
+            } else {
+                sed.insert(x);
+            }
+        }
+        return ans;
+    }
+};
