9fbba577aa25c0d78ac17596f0dd108a94669b78
Marcus <e0014884@u.nus.edu>
2022-07-18 00:11:43 +0800

    Time: 0 ms (100.00%), Space: 7.9 MB (61.89%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size()==1) return nums[0];
+        int n=nums.size();
+        int curMax=0, prevMax=0, prevMax2=0, curMax2=0;
+        for (int i=0; i<n; i++) {
+            if (i>=0 && i<n-1) {
+                int temp=curMax;
+                curMax=max(prevMax+nums[i],curMax);
+                prevMax=temp;
+            }
+            if (i>0) {
+                int temp2=curMax2;
+                curMax2=max(prevMax2+nums[i],curMax2);
+                prevMax2=temp2;
+            }
+        }
+        return max(curMax,curMax2);
+    }
+};
