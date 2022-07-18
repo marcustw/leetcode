86cfea00a57afe50cac71e99a0726dbb7c9b3c71
Marcus <e0014884@u.nus.edu>
2022-07-18 09:51:22 +0800

    Time: 2 ms (61.29%), Space: 7.8 MB (61.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -5,12 +5,12 @@ public:
         int n=nums.size();
         int curMax=0, prevMax=0, prevMax2=0, curMax2=0;
         for (int i=0; i<n; i++) {
-            if (i>=0 && i<n-1) {
+            if (i>=0 && i<n-1) { //skip last (circular array)
                 int temp=curMax;
                 curMax=max(prevMax+nums[i],curMax);
                 prevMax=temp;
             }
-            if (i>0) {
+            if (i>0) { // skip first (circular array)
                 int temp2=curMax2;
                 curMax2=max(prevMax2+nums[i],curMax2);
                 prevMax2=temp2;
