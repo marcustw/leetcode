737d6d53c03bb94f5c52ebe79e14afcb39f34aa1
Marcus <e0014884@u.nus.edu>
2022-07-20 22:19:59 +0800

    Time: 702 ms (94.12%), Space: 150.9 MB (68.11%) - LeetHub

---
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(), m=matrix[0].size();
+        int ans=0;
+        // prefix sum
+        for (int i=0; i<n; i++) {
+            for (int j=1; j<m; j++) matrix[i][j]+=matrix[i][j-1];
+        }
+        
+        for (int i=0; i<m; i++) {
+            for (int j=i; j<m; j++) {
+                unordered_map<int,int> m;
+                m[0]=1;
+                int cur=0;
+                for (int k=0; k<n; k++) {
+                    cur+=matrix[k][j] - (i>0 ? matrix[k][i-1] : 0);
+                    ans+=m.count(cur-target) != 0 ? m[cur-target] : 0;
+                    m[cur]++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
