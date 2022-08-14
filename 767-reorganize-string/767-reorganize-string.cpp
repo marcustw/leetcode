cee14b754f3dc02957510cb32f30b0ee9ba03404
Marcus <e0014884@u.nus.edu>
2022-08-14 19:21:58 +0800

    Time: 5 ms (38.30%), Space: 6.7 MB (6.29%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n=s.length();
+        vector<int> counts(26,0);
+        int maxFreq=0;
+        for (char& c : s) {
+            counts[c-'a']++;
+            maxFreq=max(maxFreq,counts[c-'a']);
+        }
+        priority_queue<pair<int,char>> pq;
+        for (int i=0; i<26; i++) {
+            pq.push({counts[i],i+'a'});
+        }
+        vector<string> strs(maxFreq,"");
+        int q=0;
+        while (!pq.empty()) {
+            pair<int,char> p=pq.top();
+            pq.pop();
+            for (int i=0; i<p.first; i++) {
+                strs[q++]+=p.second;
+                q%=maxFreq;
+            }
+        }
+        string ans=strs[0];
+        for (int i=1; i<maxFreq; i++) {
+            if (ans[ans.length()-1]==strs[i][0]) return "";
+            ans+=strs[i];
+        }
+        return ans;
+    }
+};
