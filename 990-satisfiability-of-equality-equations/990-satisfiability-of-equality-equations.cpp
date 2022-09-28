ada788a9812919222b3e42c3f1d9647f35952408
Marcus <e0014884@u.nus.edu>
2022-09-28 23:14:14 +0800

    Time: 7 ms (83.48%), Space: 12.1 MB (12.25%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -8,12 +8,15 @@ class Solution {
 public:
     bool equationsPossible(vector<string>& equations) {
         for (int i=0; i<26; i++) uf[i] = i;
+        vector<string> nots;
         for (string& eqn : equations) {
             if (eqn[1] == '=') {
                 uf[find(eqn[0] - 'a')] = find(eqn[3] - 'a');
+            } else {
+                nots.push_back(eqn);
             }
         }
-        for (string& eqn : equations) {
+        for (string& eqn : nots) {
             if (eqn[1] == '!' && find(eqn[0]-'a') == find(eqn[3] - 'a')) return false;
         }
         return true;
