16f8c10db000861e2d0bd00e4882d93ea338a16c
Marcus <e0014884@u.nus.edu>
2022-10-05 01:39:51 +0800

    Time: 14 ms (66.55%), Space: 7.1 MB (96.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
-        unordered_set<char> sed(s.begin(), s.end());
-        vector<int> m(26,0), m2(26,0);
-        for (int i=0; i<s.length(); i++) {
-            m[s[i]-'a']++;
-            m2[t[i]-'a']++;
-        }
-        for (auto& c : sed) {
-            if (m[c-'a'] != m2[c-'a']) return false;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return t == s;
     }
 };
