db34a13082902703c7fe34a62c275ae93a9fa35c
Marcus <e0014884@u.nus.edu>
2022-10-09 21:35:25 +0800

    Time: 105 ms (14.94%), Space: 38.8 MB (33.49%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -10,25 +10,19 @@
  * };
  */
 class Solution {
-    int count = 0;
-    vector<int> vals;
+    unordered_set<int> s;
     
-    void populate(TreeNode* node) {
-        if (node == NULL) return;
-        vals.push_back(node->val);
-        populate(node->left);
-        populate(node->right);
+    bool recurse(TreeNode* node, int k) {
+        if (node == NULL) return false;
+        if (s.count(k - node->val) > 0) return true;
+        s.insert(node->val);
+        return recurse(node->left, k) || recurse(node->right, k);
     }
 
 public:
     bool findTarget(TreeNode* root, int k) {
-        unordered_set<int> m;
-        populate(root);
-        for (int i=0; i<vals.size(); i++) {
-            int x = vals[i];
-            if (m.count(k - x) > 0) return true;
-            m.insert(x);
-        }
-        return false;
+        if (root == NULL) return false;
+        s.insert(root->val);
+        return recurse(root->left, k) || recurse(root->right, k);
     }
 };
