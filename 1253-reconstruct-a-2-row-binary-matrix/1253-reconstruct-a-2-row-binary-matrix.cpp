9ac8764a3a3312369d468f75c8b68d8a04695ad0
Marcus <e0014884@u.nus.edu>
2022-12-04 18:20:59 +0800

    Time: 192 ms (25.51%), Space: 62.6 MB (14.29%) - LeetHub

---
+++ 1253-reconstruct-a-2-row-binary-matrix/1253-reconstruct-a-2-row-binary-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructMatrix(int U, int L, vector<int>& C) {
+        int N = C.size();
+        vector<int> c1(N, 0);
+        vector<int> c2(N, 0);
+        for (int i=0; i<N; i++) {
+            if (C[i] == 0) continue;
+            if (C[i] == 2) {
+                c1[i] = 1;
+                c2[i] = 1;
+                U--;
+                L--;            
+            }
+        }
+        for (int i=0; i<N; i++) {
+            if (C[i] == 1) {
+                if (U>0) {
+                    c1[i] = 1;
+                    U--;
+                } else {
+                    c2[i] = 1;
+                    L--;
+                }
+            }
+        }
+        cout << "U = " << U << ", L = " << L << endl;
+        if (U != 0 || L != 0) return vector<vector<int>>();
+        return {c1, c2};
+
+    }
+};
