3047ed4cc9ae876c29d9e40eac34e3ca2d740ae2
Marcus <e0014884@u.nus.edu>
2022-12-05 09:46:59 +0800

    Time: 4 ms (29.45%), Space: 6.4 MB (44.97%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.length();
+        if (n==1) return "";
+        int replaced;
+        char replacedChar;
+        for (int i=0; i<n; i++) {
+            if (palindrome[i] != 'a') {
+                replacedChar = palindrome[i];
+                palindrome[i] = 'a';
+                replaced = i;
+                break;
+            }
+            if (i == n-1) {
+                palindrome[i] = 'b';
+            }
+        }
+        unordered_set<char> s(palindrome.begin(), palindrome.end());
+        if (s.size() == 1) {
+            palindrome[replaced]=replacedChar;
+            palindrome[n-1]='b';
+        }
+        return palindrome;
+    }
+};
