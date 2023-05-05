19748868a6bff7f829981db4256450877222f700
Marcus <marcustanwei1@gmail.com>
2023-05-05 14:04:44 +0800

    Time: 98 ms (11.14%) | Memory: 17.8 MB (8.28%) - LeetSync

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,20 +1,13 @@
 class Solution:
-    def binSearch(self, nums: List[int], target: int, lo: int, hi: int) -> int:
-        while (lo<=hi):
-            mid = lo + (hi-lo)//2
-            if nums[mid] >= target:
-                hi = mid-1
-            else:
-                lo = mid+1
-        return lo
-
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        if n==0:
+        def search(lo, hi):
+            if lo>hi:
+                return [-1, -1]
+            if nums[lo] == target == nums[hi]:
+                return [lo, hi]
+            if nums[lo] <= target <= nums[hi]:
+                mid = (lo+hi)//2
+                l, r = search(lo, mid), search(mid+1, hi)
+                return max(l,r) if -1 in l+r else [l[0], r[1]]
             return [-1, -1]
-        lo = self.binSearch(nums, target, 0, n-1)
-        hi = self.binSearch(nums, target+1, lo, n-1) - 1
-        if lo < 0 or lo >= n or hi < 0 or hi >= n or nums[lo] != target or nums[hi] != target:
-            return [-1, -1]
-        return [lo, hi]
-        
+        return search(0, len(nums)-1)
