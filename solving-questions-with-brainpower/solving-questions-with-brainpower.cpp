43506079f11f337ae4742d5559a73e6658f66911
Marcus <marcustanwei1@gmail.com>
2023-05-12 19:43:24 +0800

    Time: 399 ms (25.62%) | Memory: 113.5 MB (97.98%) - LeetSync

--- solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,17 +1,12 @@
 class Solution {
-    long long mostPoints(vector<vector<int>>& qns, vector<long long>& dp, int i) {
-        if (i>=qns.size()) return 0L;
-        if (dp[i]!=-1L) return dp[i];
-        return dp[i]=qns[i][0]+mostPoints(qns, dp, i+1+qns[i][1]);
-    }
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n=questions.size();
-        long long ans=0L;
-        vector<long long> dp(n, -1L);
+        long long ans=0L, dp[200001];
+        memset(dp, 0L, sizeof(dp));
         dp[n-1]=questions[n-1][0];
         for (int i=n-2; i>=0; i--) {
-            dp[i]=max(dp[i+1], mostPoints(questions, dp, i));
+            dp[i]=max(dp[i+1], questions[i][0] + dp[questions[i][1]+i+1]);
         }
         return dp[0];
     }
