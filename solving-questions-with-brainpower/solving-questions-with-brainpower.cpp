506cfabd22c7f4ae17d061cc6a309daca66627e1
Marcus <marcustanwei1@gmail.com>
2023-05-12 19:43:44 +0800

    Time: 355 ms (91.24%) | Memory: 113.5 MB (97.08%) - LeetSync

--- solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -4,8 +4,7 @@ public:
         int n=questions.size();
         long long ans=0L, dp[200001];
         memset(dp, 0L, sizeof(dp));
-        dp[n-1]=questions[n-1][0];
-        for (int i=n-2; i>=0; i--) {
+        for (int i=n-1; i>=0; i--) {
             dp[i]=max(dp[i+1], questions[i][0] + dp[questions[i][1]+i+1]);
         }
         return dp[0];
