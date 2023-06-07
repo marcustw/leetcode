f3457f1d962efe9d41d0f9627ad99934073ccc1e
Marcus <marcustanwei1@gmail.com>
2023-06-07 09:54:17 +0800

    Time: 0 ms (100.00%) | Memory: 6.1 MB (16.51%) - LeetSync

---
+++ minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int ans=0;
+        while (a>0 || b>0 || c>0) {
+            int ba = a&1, bb = b&1, bc = c&1;
+            if (bc==0) {
+                ans += (ba+bb);
+            } else if (ba==0 && bb==0) ans++;
+
+            a>>=1;
+            b>>=1;
+            c>>=1;
+        }
+        return ans;
+    }
+};
