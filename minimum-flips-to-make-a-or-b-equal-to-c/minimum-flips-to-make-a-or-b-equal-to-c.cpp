dca4b874cf5b76d107ce2b93ca0eb2bf83ff0a97
Marcus <marcustanwei1@gmail.com>
2023-06-07 09:54:35 +0800

    Time: 0 ms (100.00%) | Memory: 5.9 MB (53.28%) - LeetSync

--- minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -4,10 +4,10 @@ public:
         int ans=0;
         while (a>0 || b>0 || c>0) {
             int ba = a&1, bb = b&1, bc = c&1;
-            if (bc==0) {
-                ans += (ba+bb);
-            } else if (ba==0 && bb==0) ans++;
 
+            if (bc==0) ans += (ba+bb);
+            else if (ba==0 && bb==0) ans++;
+            
             a>>=1;
             b>>=1;
             c>>=1;
