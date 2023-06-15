189b1cfd69ba8f75ccd9ee2decbb3608e4966a6b
Marcus <marcustanwei1@gmail.com>
2023-06-15 17:17:01 +0800

    Time: 601 ms (6.91%) | Memory: 72.8 MB (81.52%) - LeetSync

--- total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
+++ total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -1,27 +1,23 @@
 typedef pair<int, int> P;
 class Solution {
-    long long ans=0L;
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        priority_queue<P, vector<P>, greater<P>> pq;
-        int n=costs.size(), lo, hi;
-        for (lo=0; lo<candidates && lo<n; ++lo) {
-            pq.push({costs[lo], lo});
-        }
-        for (hi=n-1; hi>=n-candidates && hi>=0; hi--) {
-            pq.push({costs[hi], hi});
-        }
-        unordered_set<int> idx;
-        while (idx.size()<k) {
-            P p=pq.top(); pq.pop();
-            if (idx.find(p.second) != idx.end()) continue;
-            ans+=p.first;
-            idx.insert(p.second);
-            if (p.second<lo && lo<=hi) {
-                pq.push({costs[lo], lo++});
-            } else if (p.second>hi && lo<=hi) {
-                pq.push({costs[hi], hi--});
+        priority_queue<int, vector<int>, greater<int>> pq1, pq2;
+        int n=costs.size(), lo=0, hi=n-1;
+        long long ans=0L;
+        while (k>0) {
+            while (pq1.size()<candidates && lo<=hi) pq1.push(costs[lo++]);
+            while (pq2.size()<candidates && lo<=hi) pq2.push(costs[hi--]);
+            if (!pq1.empty() && (pq2.empty() || pq1.top() <= pq2.top())) {
+                ans+=pq1.top();
+                cout << "add pq1: " << pq1.top() << endl;
+                pq1.pop();
+            } else {
+                ans+=pq2.top();
+                cout << "add pq2: " << pq2.top() << endl;
+                pq2.pop();
             }
+            k--;
         }
         return ans;
     }
