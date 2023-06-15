8cb3bf4ed50b30bd16d294b2142aed68f8a14749
Marcus <marcustanwei1@gmail.com>
2023-06-15 12:00:45 +0800

    Time: 150 ms (84.65%) | Memory: 57.4 MB (50.72%) - LeetSync

---
+++ number-of-recent-calls/number-of-recent-calls.cpp
@@ -0,0 +1,18 @@
+class RecentCounter {
+    queue<int> q;
+public:
+    RecentCounter() {}
+    
+    // return number of requests in the past 3000 milliseconds
+    int ping(int t) {
+        while (!q.empty() && q.front()<t-3000) q.pop();
+        q.push(t);
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
