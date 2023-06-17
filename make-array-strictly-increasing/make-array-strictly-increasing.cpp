caacad935fe94f9140d0808ee9921e9fed631b2b
Marcus <marcustanwei1@gmail.com>
2023-06-18 00:59:42 +0800

    Time: 272 ms (60.46%) | Memory: 401.1 MB (5.23%) - LeetSync

--- make-array-strictly-increasing/make-array-strictly-increasing.cpp
+++ make-array-strictly-increasing/make-array-strictly-increasing.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int solve(int i,int j,int &a,int &b,int p,vector<vector<int>> &dp,vector<int> &v1,vector<int> &v2){
-        if(i==a){
-            return 0;
-        }
-        j = upper_bound(v2.begin()+j,v2.end(),p)-v2.begin();
+    int recurse(int i,int j, int p,vector<vector<int>> &dp,vector<int> &arr1,vector<int> &arr2){
+        if (i==arr1.size()) return 0;
+        int n=arr1.size();
+        j=upper_bound(arr2.begin()+j, arr2.end(), p)-arr2.begin();
         if(dp[i][j] != -1)return dp[i][j];
-        if(j==b && v1[i]<=p)return 2001;
-        int take = 2001,nottake = 2001;
-        if(j!=b)
-        take = solve(i+1,j+1,a,b,v2[j],dp,v1,v2)+1;
-        if(v1[i]>p)
-        nottake = solve(i+1,j,a,b,v1[i],dp,v1,v2);
-        return dp[i][j] = min(take,nottake);
+        if(j==arr2.size() && arr1[i]<=p)return 2001;
+        int swap=2001, noSwap=2001;
+        if(j<arr2.size())
+            swap=recurse(i+1,j+1,arr2[j],dp,arr1,arr2)+1;
+        if(arr1[i]>p)
+            noSwap=recurse(i+1,j,arr1[i],dp,arr1,arr2);
+        // cout << "i = " << i << ", idx = " << j << ", swap = " << swap << ", noSwap = " << noSwap << endl;
+        return dp[i][j]=min(swap, noSwap);
     }
     int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
         int n = arr1.size(),m=arr2.size();
         vector<vector<int>> dp(2001,vector<int>(2001,-1));
         sort(arr2.begin(),arr2.end());
-        int a= solve(0,0,n,m,-1,dp,arr1,arr2);
-        if(a>n)return -1;
-        return a;
+        int ans = recurse(0,0,-1,dp,arr1,arr2);
+        return ans>n ? -1 : ans;
     }
 };
