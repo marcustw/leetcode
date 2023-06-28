64090ab633fc6a17c304476b17c013912ef17cbf
Marcus <marcustanwei1@gmail.com>
2023-06-28 17:37:47 +0800

    Time: 488 ms (25.03%) | Memory: 163.2 MB (57.88%) - LeetSync

--- number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
+++ number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -1,8 +1,7 @@
 class Solution {
     long long MOD=1e9+7;
-    vector<vector<long long>> table;
 
-    long long dfs(vector<int>& nums) {
+    long long dfs(vector<int>& nums, vector<vector<long long>>& table) {
         int n=nums.size();
         if (n<=2) return 1;
         vector<int> leftTree, rightTree;
@@ -10,21 +9,21 @@ class Solution {
             if (nums[i]<nums[0]) leftTree.push_back(nums[i]);
             else rightTree.push_back(nums[i]);
         }
-        long long left = dfs(leftTree) % MOD;
-        long long right = dfs(rightTree) % MOD;
+        long long left = dfs(leftTree, table) % MOD;
+        long long right = dfs(rightTree, table) % MOD;
         int l=leftTree.size(), r=rightTree.size();
         return (((table[n-1][l] * left) % MOD) * right) % MOD;
     }
 public:
     int numOfWays(vector<int>& nums) {
         int n=nums.size();
-        table.resize(n+1);
+        vector<vector<long long>> table(n+1);
         for (int i=0; i<n+1; ++i) {
             table[i] = vector<long long>(i+1, 1L);
             for (int j=1; j<i; ++j)
                 table[i][j] = (table[i-1][j-1] + table[i-1][j]) % MOD;
         }
-        long long ans=dfs(nums) % MOD;
+        long long ans=dfs(nums, table) % MOD;
         return ans-1;
     }
 };
