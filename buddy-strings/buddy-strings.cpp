10bc084e83496c589f0326c3a5d069a57ff5d096
Marcus <marcustanwei1@gmail.com>
2023-07-03 16:32:21 +0800

    Time: 3 ms (78.34%) | Memory: 7.1 MB (19.51%) - LeetSync

--- buddy-strings/buddy-strings.cpp
+++ buddy-strings/buddy-strings.cpp
@@ -2,22 +2,11 @@ class Solution {
 public:
     bool buddyStrings(string s, string goal) {
         if (s.length()!=goal.length()) return false;
-        char a, b;
-        int i, count=0, same=0;
-        unordered_set<char> sed;
-        for (int q=0; q<s.length(); ++q) {
-            if (s[q]!=goal[q]) {
-                if (count==2) return false;
-                if (count==0) {
-                    a=s[q], b=goal[q];
-                    i=q;
-                } else if (a!=goal[q] || b!=s[q]) return false;
-                count++;
-            } else {
-                if (sed.find(s[q])!=sed.end()) same+=1;
-                sed.insert(s[q]);
-            }
+        if (s==goal && unordered_set<char>(s.begin(), s.end()).size()<s.length()) return true;
+        vector<int> diff;
+        for (int i=0; i<s.length(); ++i) {
+            if (s[i]!=goal[i]) diff.push_back(i);
         }
-        return count==2 || (same && count%2==0);
+        return diff.size()==2 && s[diff[0]]==goal[diff[1]] && s[diff[1]]==goal[diff[0]];
     }
 };
