681f5bc1e0c8df579b90f71705391c326cc0433f
Marcus <marcustanwei1@gmail.com>
2023-07-06 22:54:06 +0800

    Time: 51 ms (16.11%) | Memory: 28.2 MB (71.68%) - LeetSync

--- minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        // O(n) solution
         int n=nums.size(), sum=0, ans=n+1, lo=0;
         for (int i=0; i<n; ++i) {
             while (sum+nums[i]-nums[lo]>=target && lo<i) {
