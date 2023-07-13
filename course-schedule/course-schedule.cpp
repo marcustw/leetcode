488cd0979519305e27cbe3b4cdb279febd66dd69
Marcus <marcustanwei1@gmail.com>
2023-07-13 12:14:17 +0800

    Time: 17 ms (95.60%) | Memory: 13.3 MB (78.81%) - LeetSync

---
+++ course-schedule/course-schedule.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> inDegree(numCourses,0);
+        for (auto& prereq: prerequisites) {
+            graph[prereq[1]].push_back(prereq[0]);
+            inDegree[prereq[0]]++;
+        }
+        
+        queue<int> q;
+        for (int i=0; i<numCourses; i++) {
+            if (inDegree[i]==0) q.push(i);
+        }
+        int num=0;
+        while (!q.empty()) {
+            int u=q.front();
+            q.pop();
+            num++;
+            for (auto& v : graph[u]) {
+                inDegree[v]--;
+                if (!inDegree[v]) q.push(v);
+            }
+        }
+        
+        return num==numCourses;
+    }
+};
