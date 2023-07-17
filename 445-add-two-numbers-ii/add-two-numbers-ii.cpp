a781a9265bd26dfbd5b12b4eddd37cd456beab7b
Marcus <marcustanwei1@gmail.com>
2023-07-17 10:47:19 +0800

    Time: 36 ms (73.95%) | Memory: 71 MB (94.50%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -9,21 +9,24 @@
  * };
  */
 class Solution {
+    ListNode* reverse(ListNode* ll) {
+        if (ll==nullptr || ll->next==nullptr) return ll;
+        ListNode *cur=ll, *nxt=ll->next;
+        ListNode *rev=reverse(nxt);
+        cur->next=nullptr;
+        nxt->next=cur;
+        return rev;
+    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* ans=nullptr;
-        stack<ListNode*> s1, s2;
-        while (l1) s1.push(l1), l1=l1->next;
-        while (l2) s2.push(l2), l2=l2->next;
+        l1=reverse(l1); l2=reverse(l2);
         int carry=0;
-        while (!s1.empty() || !s2.empty()) {
-            if (!s1.empty()) carry+=s1.top()->val, s1.pop();
-            if (!s2.empty()) carry+=s2.top()->val, s2.pop();
-            ans = new ListNode(carry%10, ans);
+        while (l1 || l2 || carry) {
+            if (l1) carry+=l1->val, l1=l1->next;
+            if (l2) carry+=l2->val, l2=l2->next;
+            ans=new ListNode(carry%10, ans);
             carry/=10;
-        }
-        if (carry!=0) {
-            ans = new ListNode(carry, ans);
         }
         return ans;
     }
